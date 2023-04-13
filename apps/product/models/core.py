3d02a74facdfabdac228fa48c00ec3b32edc613e
Ziad Abdulaziz <elzoz495@gmail.com>
2023-04-13 14:30:34 +0200

    updated fields and products specs for fields: - eleminated unnecessary fields - separated memory specs from memory actual data
    for core:
    - updated each product with it's fields

--- apps/product/models/core.py
+++ apps/product/models/core.py
@@ -1,47 +1,26 @@
 from django.db import models
-from .fields import (
-    MainFields,
-    OperatingSystem,
-    Processor,
-    Connectivity,
-    Camera,
-    Display,
-    Battery,
-    Graphics,
-    StorageRam,
-    Hdmi
-)
+from .fields import *
 
 
 class Mobile(MainFields,
              OperatingSystem,
-             Processor,
-             Connectivity,
-             Camera,
-             Display,
-             Battery,
-             Graphics,
-             StorageRam):
+             ConnectivityTech,
+             GeneralStorage):
+    screen_size = models.DecimalField(max_digits=3, decimal_places=1)
 
     def __str__(self):
         return f"{self.product.brand} {self.model}"
 
 
-class TV(MainFields, OperatingSystem, Connectivity, Display, Graphics, Hdmi):
-    sound = models.CharField(max_length=50, null=True, blank=True)
+class TV(MainFields, ConnectivityTech, Display):
     smart_tv = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.product.brand} {self.model}"
 
 
-class Laptop(MainFields, OperatingSystem, Processor, Connectivity, Display, Graphics, Hdmi, Battery, StorageRam):
+class Laptop(MainFields, OperatingSystem, Processor, Display, Graphics, Battery, Storage):
 
     def __str__(self):
         return f"{self.product.brand} {self.model}"
 
-
-class Tablet(MainFields, OperatingSystem, Processor, Connectivity, Display, Battery, Graphics, StorageRam):
-
-    def __str__(self):
-        return f"{self.product.brand} {self.model}"
