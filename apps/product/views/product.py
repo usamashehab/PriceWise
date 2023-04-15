a9758d8c080d6dfae76bc1240f769c3fbe37b44e
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-04-15 03:06:09 +0200

    update ProductView
    update ProductView  to only retrieve and list products as updating and creating will be done by scraping scripts

--- apps/product/views/product.py
+++ apps/product/views/product.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from ..serializers import ProductSerializer
 from ..models import Product
 
 
-class ProductView(viewsets.ModelViewSet):
+class ProductView(viewsets.GenericViewSet,
+                  mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin
+                  ):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    http_method_names = ['get']
