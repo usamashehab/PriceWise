676de1cd3e64a3c397b3f2aa67d03c76c34b45d1
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-04-15 02:40:22 +0200

    register all models to admin bannel

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import (
+    Product,
+    Vendor,
+    Category,
+    Mobile,
+    TV,
+    Tablet,
+    Laptop
+)
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Vendor)
+admin.site.register(Category)
+admin.site.register(Mobile)
+admin.site.register(TV)
+admin.site.register(Tablet)
+admin.site.register(Laptop)
