5181364868ee9bf26dbabb80bb9acd44a3de8a9e
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-05-12 02:00:06 +0300

    update SearchView
    update SearchView  to fetch products if title include the search keyword

--- product/serializers/search.py
+++ product/serializers/search.py
@@ -1,22 +1,6 @@
 
 from rest_framework import serializers
-from django.contrib.postgres.search import SearchQuery, SearchRank, TrigramSimilarity
-from ..models import Product
-from django.db.models import F, Q
-from ..serializers import ProductSerializer
 
 
 class SearchSerializer(serializers.Serializer):
     search = serializers.CharField(write_only=True)
-    products = ProductSerializer(many=True, read_only=True)
-
-    def validate(self, attrs):
-        search = attrs.pop('search', None)
-        search_query = SearchQuery(search)
-        products = Product.objects.annotate(
-            similarity=TrigramSimilarity('title', search),
-            rank=SearchRank(F('search_vector'), search_query)
-        ).filter(Q(search_vector=search_query) | Q(similarity__gt=0.1)).order_by('-similarity', "-rank")
-
-        attrs['products'] = products
-        return attrs
--- product/views/search.py
+++ product/views/search.py
@@ -2,8 +2,21 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from ..models import Product
+from django.db.models import Count
 from ..serializers import SearchSerializer, ProductSerializer
+from django.contrib.postgres.search import SearchQuery, SearchRank, TrigramSimilarity
+from django.db.models import F, Q
 
+
+filter_attrs = {
+    "Mobiles": [
+        "brand" ,
+        "ram" ,
+        "storage" ,
+        "camera" ,
+    ]
+       , 
+}
 
 class SearchView(viewsets.GenericViewSet):
     queryset = Product.objects.all()
@@ -13,7 +26,17 @@ class SearchView(viewsets.GenericViewSet):
     def search(self, request, format=None):
         serializer = SearchSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        page = self.paginate_queryset(serializer.data["products"])
+        search = serializer.validated_data["search"]
+        search_query = SearchQuery(search)
+        products = Product.objects.annotate(
+            similarity=TrigramSimilarity('title', search),
+            rank=SearchRank(F('search_vector'), search_query)
+        ).filter(Q(search_vector=search_query) | Q(similarity__gt=0) | Q(title__icontains=search)).order_by('-similarity', "-rank")
+        category = products.values('category__name').annotate(
+            count=Count('category')).order_by('-count').first()
+        print(category)
+        page = self.paginate_queryset(products)
+
         if page is not None:
             # used CompanyProfileSerializer to serialize the companies query
             serializer = ProductSerializer(page, many=True)
