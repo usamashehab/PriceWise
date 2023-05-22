7c02b50fbd6b39f71a4d832be21f3e4ce9252dea
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-05-22 23:53:21 +0300

    fix bug in importing Image model

--- product/models/__init__.py
+++ product/models/__init__.py
@@ -1,9 +1,9 @@
 from .category import Category
 from .vendor import Vendor
-from .product import Product, Price
+from .product import Product, Price, Image
 from .core import (
     Mobile,
     TV,
     Tablet,
-    Laptop
+    Laptop,
 )
