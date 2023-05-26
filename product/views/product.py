b3e299646daad0b8938cf656084087598990a991
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-05-26 22:15:01 +0300

    update the path of products list to be 'product/popular/'

--- product/views/product.py
+++ product/views/product.py
@@ -10,7 +10,6 @@ from django.db.models import F, FloatField
 
 
 class ProductView(viewsets.GenericViewSet,
-                  mixins.ListModelMixin,
                   mixins.RetrieveModelMixin
                   ):
     serializer_class = ProductSerializer
@@ -64,3 +63,15 @@ class ProductView(viewsets.GenericViewSet,
 
         serializer = self.get_serializer(products, many=True)
         return Response(serializer.data, status=200)
+
+    @action(methods=['get'], detail=False, url_path='popular')
+    def popular(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
