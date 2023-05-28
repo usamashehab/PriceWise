65eed61d8c1e1b4eaf9b94a1025cfe97bbda36d8
usamashehab <53179532+usamashehab@users.noreply.github.com>
2023-05-28 22:02:57 +0300

    update the paggination to include 50 object per page

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 50,
 }
 
 # JWT ------------------------------------------------------------------------------------------------
